795769573b18f9500551e2348a9e942a2634144c
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-11-19 20:27:48 -0600

    cli arg parser

---
+++ src/main/scala/parsing/Parsers.scala
@@ -0,0 +1,33 @@
+package org.aranadedoros
+package parsing
+
+import model.Model.{EntryKey, Flag, Site}
+
+object Parsers {
+  object FlagParser:
+
+    def parse(args: List[String]): Either[String, Flag] =
+      args match
+        case "--add" :: rawSite :: rawKey :: Nil =>
+          for
+            site <- Site.fromRaw(rawSite)
+            key  <- EntryKey.fromRaw(rawKey)
+          yield Flag.Add(site, key)
+        case "--del" :: rawSite :: rawKey :: Nil =>
+          for
+            site <- Site.fromRaw(rawSite)
+            key  <- EntryKey.fromRaw(rawKey)
+          yield Flag.Delete(site, key)
+        case "--list" :: Nil =>
+          Right(Flag.ListAll)
+        case "--search" :: rawKey :: Nil =>
+          EntryKey.fromRaw(rawKey).map(Flag.Search.apply)
+        case "--init" :: Nil =>
+          Right(Flag.Init)
+        case "--help" :: Nil | Nil =>
+          Right(Flag.Help)
+
+        case other =>
+          Left(s"Unknown command: ${other.mkString(" ")}")
+
+}
